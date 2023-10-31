e47b9bac73fab12a9655737d2e728942e425b57b
Saidakramov <akbarsaidakramov@gmail.com>
2023-10-31 12:36:15 -0700

    adding module challenge

---
+++ PH_Challenge/Employee_Database_challenge.sql
@@ -0,0 +1,49 @@
+-- Getting the number of retiring employees by title
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+-- INTO retirement_titles
+FROM employees e
+INNER JOIN titles t ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY 1;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+-- INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM titles;
+SELECT * FROM employees LIMIT 10;
+
+-- Create a retireing titles
+SELECT COUNT(title), 
+	title
+-- INTO retiring_titles
+FROM unique_titles
+GROUP BY 2
+ORDER BY 1 DESC;
+
+-- Create mentorship eligibily program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+--INTO mentorship_eligibility
+FROM employees e
+JOIN dept_emp de ON (e.emp_no = de.emp_no)
+JOIN titles t ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') 
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
